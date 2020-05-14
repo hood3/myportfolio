749f77ef0bb0da490386286c749f28cece28fb58
Byron Hood <hood3@users.noreply.github.com>
2020-05-14 10:56:41 -0500

    changes

--- src/papers/HireMe.js
+++ src/papers/HireMe.js
@@ -1,47 +1,8 @@
 import '../myStyle.scss';
 import { Link } from 'react-router-dom';
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-const HireMe = () => {  
-    const [values,setValues] = useState ({//State object values with method setValues
-        name:'',
-        email:'',
-        WebsiteType:'',
-        buttonText:'Send'//Button text will change during events
-      });
-
-      const {name,email,WebsiteType,buttonText} = values; // Destructured to save typing
-
-      const handleChange = name => event => {   //Function to grab input values on change
-        setValues({ ...values, [name]: event.target.value });//and set the state with values
-    };
-
-    const clickSubmit = event => { //Events from button being clicked
-        event.preventDefault(); //Keep page from reloading
-        setValues({ ...values, buttonText: 'Sending' }); // Change button value while submitting
-    };
-    const quoteForm = () => (//Method containing the Registration form
-        <form>
-            <div className="form-group">
-                <label className="text-black-150 font-weight-bold">Name</label>
-                <input onChange={handleChange('name')} value={name} type="text" className="form-control" />
-            </div>
-            <div className="form-group">
-                <label className="text-black-150 font-weight-bold">Email</label>
-                <input onChange={handleChange('email')} value={email} type="email" className="form-control" />
-            </div>
-            <div className="form-group">
-                <label className="text-black-150 font-weight-bold">Website Type</label>
-                <input onChange={handleChange('WebsiteType')} value={WebsiteType} type="text" className="form-control" />
-            </div>
-            <div>
-                <button className="btn btn-primary" onClick={clickSubmit}>
-                    {buttonText}
-                </button>
-            </div>
-        </form>
-      );
+const HireMe = () => {   
 
     return( 
         <div>
@@ -90,11 +51,7 @@ const HireMe = () => {
                         Product Rating & Reviews.
                     </li>
                 </ul>                
-            </p>           
-            <div>            
-            <p className="pri"> Fill out form for a free Quote</p> 
-            {quoteForm()}                          
-            </div>              
+            </p>                    
             </div>  
             </div>   
     );
