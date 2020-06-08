deffca6a4fd49f6e2dca331bdd168bc4b51256dd
Byron Hood <hood3@users.noreply.github.com>
2020-06-08 15:24:59 -0500

    chnges

--- src/papers/viewResume.js
+++ src/papers/viewResume.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import '../myStyle.scss';
-//import Resume from '../assets/Resume.jpg';
+import resume from '../assets/resume.jpg';
 import { Link } from 'react-router-dom';
 import Scroll from'../components/Scroll';
 
@@ -12,7 +12,7 @@ class viewResume extends Component {
                     <Link class="cta-btn cta-btn--hero" to='/'>Portfolio</Link> 
                 </div>
                 <Scroll>
-                <img  className="resume" src={Resume} alt="this is my resume" height="2000px" width="1400px" />
+                <img  className="resume" src={resume} alt="this is my resume" height="2000px" width="1400px" />
                 </Scroll>
             </div>      
     );
