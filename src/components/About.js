ae59815befdafffa901b7d7cd75f96c0badc185f
Byron Hood <hood3@users.noreply.github.com>
2020-12-20 10:19:24 -0600

    changes made

--- src/components/About.js
+++ src/components/About.js
@@ -24,14 +24,17 @@ const About = () => {
                         self-employed, I built a Clientele, formed a team, handled all concerns, and worked on site, 
                         to the oil and gas industry where I became lead MWD Engineer within two years.
                         Having found a keen interest in computers, I enrolled at Oklahoma Northeastern State University
-                        to gain more computer knowledge, and obtain a Master of Science Degree in Computer Science. 
+                        to gain more computer knowledge, and obtain a Bachelor of Science Degree in Computer Science. 
                         Because of my obsessive attention to detail, my unequivocal desire to create,
                         and my mission-driven work ethic to learn new technology each day, I enjoy web developing. Fast load times and 
                         lag-free interaction are high priorities. REACT is a favorite to use whenever possible for front-end 
-                        development so my layouts will be dynamic and will fit any device’s screen, large or small.
-                        I am committed to my passion to continue learning, to help others, and to try to solve problems.
-                        Coordinating projects with team members in college and previous employments has been profuse.Therefore,
-                        I am eager for an opportunity to make an impact at a fast-growing, high-expectation company.                         
+                        development so my layouts will be dynamic and will fit any device’s screen, large or small. As a systems administrator, 
+                        one of the things I love is that if I am doing my job right, no one will ever know I was there, everything will just work
+                        perfectly and smoothly. Clients will be free to do what they do best without ever having to worry about their servers, or 
+                        even fully understand how they work. I love being able to provide that kind of peace of mind for my clients, and love 
+                        watching them be productive thanks to my efforts. I am committed to my passion to continue learning, to help others, and 
+                        to try to solve problems. Coordinating projects with team members in college and previous employments has been profuse.
+                        Therefore, I am eager for an opportunity to make an impact at a fast-growing, high-expectation company.                         
                     </p>  
                     <span class="d-flex mt-3">  
                     <Link class="cta-btn cta-btn--hero" to='/resume'>View Resume</Link>    
